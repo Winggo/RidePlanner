b023954664628004e67ee4ea1be9f60e05a1a47f
mjpon <mjpon@ucsc.edu>
2018-10-29 14:48:55 -0700

    more ui updates

--- app/src/main/java/com/cmps121/rideplanner/ui/yourcar/YourCarViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.cmps121.rideplanner.ui.yourcar;
-
-import android.arch.lifecycle.ViewModel;
-
-public class YourCarViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/cmps121/rideplanner/yourCar.java
+++ app/src/main/java/com/cmps121/rideplanner/yourCar.java
@@ -3,18 +3,11 @@ package com.cmps121.rideplanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.cmps121.rideplanner.ui.yourcar.YourCarFragment;
-
 public class yourCar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.your_car_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, YourCarFragment.newInstance())
-                    .commitNow();
-        }
+        setContentView(R.layout.activity_your_car);
     }
 }
