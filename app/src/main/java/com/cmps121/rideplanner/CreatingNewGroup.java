3a4774c5722d2c42e66839e860a3649b8fab559b
mjpon <mjpon@ucsc.edu>
2018-10-28 21:08:18 -0700

    Adding more UI

---
+++ app/src/main/java/com/cmps121/rideplanner/CreatingNewGroup.java
@@ -0,0 +1,13 @@
+package com.cmps121.rideplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatingNewGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creating_new_group);
+    }
+}
---
+++ app/src/main/java/com/cmps121/rideplanner/ui/yourcar/YourCarViewModel.java
@@ -0,0 +1,7 @@
+package com.cmps121.rideplanner.ui.yourcar;
+
+import android.arch.lifecycle.ViewModel;
+
+public class YourCarViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
