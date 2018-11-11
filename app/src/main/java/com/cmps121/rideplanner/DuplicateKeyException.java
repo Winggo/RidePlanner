deedd8e79f3f39070e835aae692b32e68e1eb3e2
mjpon <mjpon@ucsc.edu>
2018-11-11 15:07:43 -0800

    fixed merges

---
+++ app/src/main/java/com/cmps121/rideplanner/DuplicateKeyException.java
@@ -0,0 +1,7 @@
+package com.cmps121.rideplanner;//-----------------------------------------------------------------------------
+// DuplicateKeyException.java
+//-----------------------------------------------------------------------------
+
+public class DuplicateKeyException extends RuntimeException{
+    public DuplicateKeyException(String s){ super(s); }
+}
---
+++ app/src/main/java/com/cmps121/rideplanner/KeyNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cmps121.rideplanner;
+
+//-----------------------------------------------------------------------------
+// KeyNotFoundException.java
+//-----------------------------------------------------------------------------
+
+public class KeyNotFoundException extends RuntimeException{
+    public KeyNotFoundException(String s){ super(s);}
+}
